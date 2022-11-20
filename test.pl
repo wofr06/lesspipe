fe3c97daa35fe6a39340a3803186d79afacddbc6
wf <wp.friebel@gmail.com>
2022-11-20 16:01:36 +0100

    revert file name test (109)

--- test.pl
+++ test.pl
@@ -443,7 +443,7 @@ LESS= less $T/tests/a-r-R.pl		# name contains -r or -R git #78
 less $T/tests/test_zip:non-existent-file	# nonexisting file in a zip archive git issue #1
 ~ 
 less $T/tests/test\ \;\'\"\[\(\{ok		# file name with chars such as ", ' ...
-c test
+= test
 less tests/special.tgz:test\ \;\'\"\[\(\{ok	# archive having a file with chars from [ ;"'] etc. in the name
 = test
 less $T/tests/test\[a\]\(b\)\{c\}.zip	# file name with parens, brackets, braces git issue 69
