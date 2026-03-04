6f284282d9438f4b25feab051b3576194315a36c
Wolfgang Friebel <wp.friebel@gmail.com>
2026-03-04 23:18:21 +0100

    suppress error msg in lesspipe

--- lesspipe.sh
+++ lesspipe.sh
@@ -431,8 +431,8 @@ has_colorizer () {
 	[[ $prog == vimcolor ]] && ! has_cmd vim && ! has_cmd nvim && prog=
 
 	for i in nvimpager bat batcat pygmentize source-highlight vim nvim code2color ; do
-		[[ -z $prog ]] && has_cmd "$i" ]] && prog=$i
-		[[ $prog == "$i" ]] && ! has_cmd "$prog" ]] && prog=
+		[[ -z $prog ]] && has_cmd "$i" && prog=$i
+		[[ $prog == "$i" ]] && ! has_cmd "$prog" && prog=
 	done
 	[[ $prog == "*vim" ]] && prog=vimcolor
 	[[ "$2" =~ ^[0-9]*$ || -z "$2" ]] || lang=$2
